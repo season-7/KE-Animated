fdb51d4c4becacc647fc0b30c4c91cd89ad1a023
olagz <olagodavivid@gmail.com>
2016-07-12 16:42:40 +0300

    updates of the css, index and the js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+document.getElementById('button').onclick = (function() {
+    document.getElementsByTagName('audio')[0].play();
+    document.getElementsByTagName('span')[0].innerHTML = 'Click meception!';
+    return false;
+});
+
