7fc39a0ff51c15fcde2352345963fd99f01cdbcb
olagz <olagodavivid@gmail.com>
2016-07-12 17:57:39 +0300

    index, css and js files updated

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).foundation()
 document.getElementById('button').onclick = (function() {
     document.getElementsByTagName('audio')[0].play();
-    document.getElementsByTagName('span')[0].innerHTML = 'Click meception!';
+    document.getElementsByTagName('span')[0].innerHTML = 'The Best!';
     return false;
 });
 
